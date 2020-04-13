e43ddd1e3ab58717d9b31502580613445bc16819
Slime_hatena <git@aki-memo.net>
2020-04-14 01:12:53 +0900

    chore: 開発時にデバッグしながらviewを変更できるように

--- OngekiMusics/Startup.cs
+++ OngekiMusics/Startup.cs
@@ -14,7 +14,11 @@ namespace OngekiMusics {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+#if DEBUG
+            .AddRazorRuntimeCompilation()
+#endif
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
