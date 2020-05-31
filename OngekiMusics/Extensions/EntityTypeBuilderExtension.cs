305879bd61fefc9b6a50720926eb5666c4793544
Slime_hatena <git@aki-memo.net>
2020-05-31 18:08:03 +0900

    refactor: 一旦変数化してから処理を行うように

--- OngekiMusics/Extensions/EntityTypeBuilderExtension.cs
+++ OngekiMusics/Extensions/EntityTypeBuilderExtension.cs
@@ -9,13 +9,15 @@ namespace OngekiMusics.Extensions {
         /// CreatedAtは初回のみ、UpdatedAtは更新時に現在時刻をセットするよう設定します。
         /// </summary>
         public static EntityTypeBuilder<TEntity> UseTimestampedProperty<TEntity>(this EntityTypeBuilder<TEntity> entity) where TEntity : class, ITimestampable {
-            entity.Property(d => d.CreatedAt).ValueGeneratedOnAdd();
-            entity.Property(d => d.CreatedAt).Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
-            entity.Property(d => d.CreatedAt).Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
+            var createdAt = entity.Property(d => d.CreatedAt);
+            createdAt.ValueGeneratedOnAdd();
+            createdAt.Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
+            createdAt.Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
 
-            entity.Property(d => d.UpdatedAt).ValueGeneratedOnAddOrUpdate();
-            entity.Property(d => d.UpdatedAt).Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
-            entity.Property(d => d.UpdatedAt).Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
+            var updatedAt = entity.Property(d => d.UpdatedAt);
+            updatedAt.ValueGeneratedOnAddOrUpdate();
+            updatedAt.Metadata.SetBeforeSaveBehavior(PropertySaveBehavior.Ignore);
+            updatedAt.Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
 
             return entity;
         }
