310f4868bfad0c5217a02030d4eb91e9ff4fb4f5
Slime_hatena <git@aki-memo.net>
2020-06-12 01:55:40 +0900

    fix(entities): 明示的にAutoIncrementを指定

--- OngekiMusics/Entities/SeasonalVersion.cs
+++ OngekiMusics/Entities/SeasonalVersion.cs
@@ -1,8 +1,10 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OngekiMusics.Entities {
     public class SeasonalVersion : ITimestampable {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
